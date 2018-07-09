36f8867dae85a7c1c9b27f95bb8634e97339f6d6
Cong Zhang <congzhangzh@gmail.com>
2018-07-09 19:20:47 +0800

    minor fix for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from distutils.core import setup
 setup(
   name = 'uproxy',
   packages = ['uproxy'],
-  version = '0.3.99.3',
+  version = '0.3.99.4',
   description = 'A both ipv4/ipv6 suppported http proxy server',
+  long_description = open('readme.rst').read(), # need to close?
   author = 'Cong Zhang',
   author_email = 'congzhangzh@gmail.com',
-  url = 'https://github.com/congzhangzh/u-proxy',
-  download_url = 'https://github.com/congzhangzh/u-proxy/0.3.99',
+  url = 'https://github.com/medlab/u-proxy',
   keywords = ['http','https', 'ipv4/ipv6', 'proxy'],
-  classifiers = [],
+  classifiers=[
+    'License :: OSI Approved :: BSD License',
+    'Development Status :: 6 - Mature',
+    'Environment :: Console',
+    'Topic :: Utilities',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
 )
